cf6241f3310f30052432eaf0ee0bc93412e35deb
Aarti Singh <aarti.s2014@gmail.com>
2023-06-26 01:57:08 -0500

    part2_2

---
+++ Easy_difficulty/merge_two_lists.py
@@ -0,0 +1,38 @@
+
+"""
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+"""
+
+def mergeTwoLists(self, list1, list2):
+    """
+    :type list1: Optional[ListNode]
+    :type list2: Optional[ListNode]
+    :rtype: Optional[ListNode]
+    """
+
+    dummylist = ListNode()
+    tail = dummylist
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next
+
+    if list1:
+        tail.next = list1
+    elif list2:
+        tail.next = list2
+
+    return dummylist.next
+
